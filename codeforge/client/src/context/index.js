0070010244a4c71e7122f3a21a6b62cf56ed34df
goboto57-pixel <goboto57@gmail.com>
2026-08-28 23:33:14 +0500

    Delete codeforge directory

--- codeforge/client/src/context/index.js
+++
@@ -1,5 +0,0 @@
-export { ChatProvider, useChat } from "./ChatContext.jsx";
-export { FilesProvider, useFiles } from "./FilesContext.jsx";
-export { SessionsProvider, useSessions } from "./SessionsContext.jsx";
-export { UIProvider, useUI } from "./UIContext.jsx";
-export { SettingsProvider, useSettings, MODELS, MODES } from "./SettingsContext.jsx";
